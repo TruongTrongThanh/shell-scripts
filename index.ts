9ee54794673181914b0d97ed4cab77ef4d65db39
Thanh <triplet.vinci@gmail.com>
2026-05-21 18:34:49 +0700

    init

---
+++ index.ts
@@ -0,0 +1,8 @@
+import type { ExtensionAPI } from "@earendil-works/pi-coding-agent";
+import { Type } from "@sinclair/typebox";
+
+// ── Extension Factory ───────────────────────────────────────────────────────
+
+export default function (pi: ExtensionAPI) {
+  
+}
